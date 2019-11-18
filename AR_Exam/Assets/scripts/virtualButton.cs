b22470209065ce505e7728150fb6d9b91c96229a
newlusifer <46846655+newlusifer@users.noreply.github.com>
2019-11-19 04:47:43 +0700

    arExam_SingleTarget&CubiodTarget

---
+++ AR_Exam/Assets/scripts/virtualButton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class virtualButton : MonoBehaviour,IVirtualButtonEventHandler
+{
+    public GameObject objBTN;
+    public GameObject Model;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        objBTN = GameObject.Find("playWithBtn");
+        objBTN.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        Model.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        Model.SetActive(true);
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        Model.SetActive(false);
+    }
+}
